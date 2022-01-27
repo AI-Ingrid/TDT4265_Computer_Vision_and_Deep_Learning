4d5d9f67a88090f217346ed6fbd58566b581a549
ikolderu <ikolderu@cisco.com>
2022-01-27 17:07:31 +0100

    2b maybe done?

--- assignment1/task2.py
+++ assignment1/task2.py
@@ -35,7 +35,11 @@ class LogisticTrainer(BaseTrainer):
             loss value (float) on batch
         """
         # TODO: Implement this function (task 2b)
-        loss = 0
+        self.model.w = np.zeros((785, 1))
+        outputs = self.model.forward(X_batch)
+        self.model.backward(X_batch, outputs, Y_batch)
+        self.model.w += - self.learning_rate * self.model.grad
+        loss = cross_entropy_loss(Y_batch, outputs)
         return loss
 
     def validation_step(self):
