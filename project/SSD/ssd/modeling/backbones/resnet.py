1f9568edb6d44c9c5512395ecee4b20e80986c4a
ikolderu <ikolderu@cisco.com>
2022-04-28 11:53:37 +0200

    WIP resnet after videotips

--- project/SSD/ssd/modeling/backbones/resnet.py
+++ project/SSD/ssd/modeling/backbones/resnet.py
@@ -3,6 +3,25 @@ from typing import Tuple, List
 from torch import nn
 import torchvision.models as models
 
+class FirstLayer(nn.Sequential):
+    def __init__(self, model):
+        super().__init__(
+        model.conv1(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False),
+        model.bn1(64, eps=1e-05, momentum=0.1, affine=True, track_running_status=True),
+        model.relu(inplace=True),
+        model.maxpool(kernal_size=3, stride=2,padding=1, dilation=1, ceil_mode=False)
+        )
+
+class LastLayer(nn.Sequential):
+    def __init__(self, model):
+        super().__init__(
+            model.relu(),
+            model.conv1(128, 128 , kernel_size=3, stride=1, padding=1),
+            model.relu(),
+            model.conv1(128, 64, kernel_size=4, stride=2, padding=1),
+            model.relu(),
+        )
+
 class ResNet(torch.nn.Module):
     """
     This is a basic backbone for SSD.
@@ -22,12 +41,18 @@ class ResNet(torch.nn.Module):
         self.out_channels = output_channels
         self.output_feature_shape = output_feature_sizes
         
+        self.model = torch.torchvision.resnet34(pratrained=True)
         self.resnet = nn.Sequential(
-            *list(models.resnet101(pretrained=True).children()[:-2])
-        )
+            FirstLayer(self.model),
+            *list(torch.torchvision.resnet34(pretrained=True).children())[4:-2],
+            LastLayer(self.model),
+            )
+        print(self.resnet.model())
         for param in self.resnet.parameters():
             param.requires_grad = False
+        
         # Create a FPN with all the outputs
+        self.feature_pyramid_net = torch.torchvision.ops.FeaturesPyramidNetwork()
 
     def forward(self, x):
         """
@@ -44,18 +69,14 @@ class ResNet(torch.nn.Module):
         """
         out_features = []
         # Peform forward on each layer in the network
-        # Might be an error here when we are going through the layers
-        
-        # Start at layer 0 and go through all layers
-        # input into layer0
         for layer in self.resnet: # check what "layer" actually is, might be iterating throug something else
             x = layer(x)
-            # Add all outputs maybe to a dictionary
+            # Save all by adding them to a list 
             out_features.append(x)
         
         # Add the output of all layers and put it into the FPN
-        # Return a list of all output features from teh FPN 
-
+        out_features = self.feature_pyramid_net(out_features)
+    
         for idx, feature in enumerate(out_features):
             out_channel = self.out_channels[idx]
             h, w = self.output_feature_shape[idx]
@@ -64,5 +85,7 @@ class ResNet(torch.nn.Module):
                 f"Expected shape: {expected_shape}, got: {feature.shape[1:]} at output IDX: {idx}"
         assert len(out_features) == len(self.output_feature_shape),\
             f"Expected that the length of the outputted features to be: {len(self.output_feature_shape)}, but it was: {len(out_features)}"
+        
+        # Return a list/typle of all output features from the FPN 
         return tuple(out_features)
 
